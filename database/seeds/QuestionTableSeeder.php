bc648d3bce0de361561fe6e9fddf6ff6fe2dba35
hoa44 <hoa44@njit.edu>
2019-04-20 14:37:32 -0400

    Added Home page route and View to show extras seeded questions.

--- database/seeds/QuestionTableSeeder.php
+++ database/seeds/QuestionTableSeeder.php
@@ -13,12 +13,14 @@ class QuestionTableSeeder extends Seeder
      */
     public function run()
     {
-        $users = User::all();
-
-        $users->each(function ($user) {
-            $question = factory(Question::class)->make();
-            $question->user()->associate($user);
-            $question->save();
-        });
+        //$users = User::all();
+        $users = User::inRandomOrder();
+        for ($i = 1; $i <= 20; $i++) {
+            $users->each(function ($user) {
+                $question = factory(Question::class)->make();
+                $question->user()->associate($user);
+                $question->save();
+            });
+        }
     }
 }
